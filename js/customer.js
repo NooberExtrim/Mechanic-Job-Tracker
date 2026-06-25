0590b7c3107a6bb613aa26c1e529708d4001f453
Mathias <mathigi@stud.ntnu.no>
2026-06-25 22:49:26 +0200

    feat: add js for job creation in memory with display of jobs

---
+++ js/customer.js
@@ -0,0 +1,57 @@
+const submitBtn = document.getElementById("submitBtn");
+const jobList = document.getElementById("jobList");
+
+if (!submitBtn || !jobList) {
+    console.error("HTML elements not found.");
+}
+
+// in-memory sotrage of jobs
+let jobs = [];
+
+
+// On-click button
+submitBtn.addEventListener("click", function () {
+
+    const name = document.getElementById("name").value;
+    const carModel = document.getElementById("carModel").value;
+    const description = document.getElementById("description").value;
+
+    if(!name || !carModel || !description) {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    const job = {
+        id: Date.now(),
+        name: name,
+        carModel: carModel,
+        description: description,
+        status: "waiting"
+    };
+
+    jobs.push(job);
+
+    renderJobs();
+
+    // Clear document
+    document.getElementById("name").value = "";
+    document.getElementById("carModel").value = "";
+    document.getElementById("description").value = "";
+});
+
+function renderJobs() {
+    jobList.innerHTML = "";
+
+    jobs.forEach(job => {
+        const div = document.createElement("div");
+        div.classList.add("job");
+
+        div.innerHTML = `
+            <h3>${job.name} - ${job.carModel}</h3>
+            <p>${job.description}</p>
+            <small>Status: ${job.status}</small>
+        `;
+
+        jobList.appendChild(div);
+    });
+}
